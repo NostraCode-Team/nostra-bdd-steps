9dd1e36a4a2459bed8c7c7ca5ed24721828ca231
lunareraze <sapitridina88@gmail.com>
2023-09-27 20:15:13 +0700

    add tap widget with text on bdd_options.yaml

--- lib/step/i_see_custom_widgets.dart
+++ lib/step/i_see_custom_widgets.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // import 'package:test_basic/widgets/container_x.dart';
-//
+//* belum berhasil
 Future<void> iSeeCustomWidgets(
   WidgetTester tester,
   Widget customWidget,
